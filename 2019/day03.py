a6fd89613a64e7ff5b15ff33cce08c3c99ecf670
Dominik Beutel <dominik.beutel@kit.edu>
2020-12-21 18:04:36 +0100

    2019 Days 3 and 4

---
+++ 2019/day03.py
@@ -0,0 +1,47 @@
+with open('input03.txt', 'r') as f:
+    wire = f.readlines()
+
+def move(pos, dir):
+    if dir == 'R':
+        return pos[0] + 1, pos[1]
+    if dir == 'L':
+        return pos[0] - 1, pos[1]
+    if dir == 'U':
+        return pos[0], pos[1] + 1
+    if dir == 'D':
+        return pos[0], pos[1] - 1
+
+candidates = {}
+
+pos = (0, 0)
+steps = 0
+for c in wire[0].split(','):
+    dir = c[0]
+    length = int(c[1:])
+    while length > 0:
+        length -= 1
+        steps += 1
+        pos = move(pos, dir)
+        if pos not in candidates:
+            candidates[pos] = steps
+
+nearest = 999_999
+shortest = 999_999
+pos = (0, 0)#
+steps = 0
+for c in wire[1].split(','):
+    dir = c[0]
+    length = int(c[1:])
+    while length > 0:
+        # print(dir, ' ', length)
+        length -= 1
+        steps += 1
+        pos = move(pos, dir)
+        if pos in candidates:
+            manhattan = abs(pos[0]) + abs(pos[1])
+            nearest = manhattan if manhattan < nearest else nearest
+            totalsteps = steps + candidates[pos]
+            shortest = totalsteps if totalsteps < shortest else shortest
+
+print(nearest)
+print(shortest)
---
+++ 2019/day04.py
@@ -0,0 +1,52 @@
+
+def nvalid(intrvl, pos, prevdigit=-1, doubled=False, atmin=True, atmax=True):
+    start = max((intrvl[0] // (10 ** pos)) % 10, prevdigit) if atmin else prevdigit
+    stop = (intrvl[1] // (10 ** pos)) % 10 if atmax else 9
+    if pos == 0:
+        if doubled:
+            return stop - start + 1
+        elif prevdigit in range(start, stop + 1):
+            return 1
+        return 0
+    total = 0
+    for i in range(start, stop + 1):
+        nowdoubled = doubled or prevdigit == i
+        nowatmin = atmin and i == start
+        nowatmax = atmax and i == stop
+        total += nvalid(intrvl, pos - 1, i, nowdoubled, nowatmin, nowatmax)
+    return total
+
+def nvalid2(intrvl, pos, prevdigit=-1, doubled=0, atmin=True, atmax=True):
+    # doubled = 0 -> single digit
+    # doubled = 1 -> vulnerable double (the only valid double is at the end)
+    # doubled = 2 -> overshooted (triple...)
+    # doubled = 3 -> safe double
+    start = max((intrvl[0] // (10 ** pos)) % 10, prevdigit) if atmin else prevdigit
+    stop = (intrvl[1] // (10 ** pos)) % 10 if atmax else 9
+    if pos == 0:
+        if doubled in (1, 3):
+            if doubled == 1 and prevdigit in range(start, stop + 1):
+                return stop - start
+            return stop - start + 1
+        elif doubled == 0 and prevdigit in range(start, stop + 1):
+            return 1
+        return 0
+    total = 0
+    for i in range(start, stop + 1):
+        if doubled in (0, 1, 2):
+            if prevdigit == i:
+                nowdoubled = min(doubled + 1, 2)
+            elif doubled == 1:
+                nowdoubled = 3
+            else:
+                nowdoubled = 0
+        else:
+            nowdoubled = doubled
+        nowatmin = atmin and i == start
+        nowatmax = atmax and i == stop
+        total += nvalid2(intrvl, pos - 1, i, nowdoubled, nowatmin, nowatmax)
+    return total
+
+intrvl = (246515, 739105)
+print(nvalid(intrvl, len(str(intrvl[1])) - 1))
+print(nvalid2(intrvl, len(str(intrvl[1])) - 1))
