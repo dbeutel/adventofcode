717a37d92c4121311c1ff046b717b7143ffcad46
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-03 12:44:47 +0100

    Slightly nicer

--- 2022/day03.py
+++ 2022/day03.py
@@ -2,13 +2,13 @@ prio = 0
 with open("input03.txt", "r") as fobj:
     for line in fobj:
         line = line.strip()
-        item = next(iter(set(line[: len(line) // 2]) & set(line[len(line) // 2 :])))
+        item = (set(line[: len(line) // 2]) & set(line[len(line) // 2 :])).pop()
         prio += ord(item) - (38 if item.isupper() else 96)
 print(prio)
 
 prio = 0
 with open("input03.txt", "r") as fobj:
     for line in fobj:
-        item = next(iter(set(line.strip()) & set(next(fobj)) & set(next(fobj))))
+        item = (set(line.strip()) & set(next(fobj)) & set(next(fobj))).pop()
         prio += ord(item) - (38 if item.isupper() else 96)
 print(prio)
