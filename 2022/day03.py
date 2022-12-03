1b0a2c0cb76d56cdf12041dce625c474e689f0c0
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-03 13:15:22 +0100

    Day 03 as (almost) a single print(sum(...)) per problem

--- 2022/day03.py
+++ 2022/day03.py
@@ -1,14 +1,17 @@
-prio = 0
-with open("input03.txt", "r") as fobj:
-    for line in fobj:
-        line = line.strip()
-        item = (set(line[: len(line) // 2]) & set(line[len(line) // 2 :])).pop()
-        prio += ord(item) - (38 if item.isupper() else 96)
-print(prio)
+prio = lambda item: ord(item) - (38 if item.isupper() else 96)
 
-prio = 0
 with open("input03.txt", "r") as fobj:
-    for line in fobj:
-        item = (set(line.strip()) & set(next(fobj)) & set(next(fobj))).pop()
-        prio += ord(item) - (38 if item.isupper() else 96)
-print(prio)
+    print(
+        sum(
+            prio((set(line[: len(line) // 2]) & set(line[len(line) // 2 :])).pop())
+            for line in fobj
+        )
+    )
+
+with open("input03.txt", "r") as fobj:
+    print(
+        sum(
+            prio((set(line.strip()) & set(next(fobj)) & set(next(fobj))).pop())
+            for line in fobj
+        )
+    )
