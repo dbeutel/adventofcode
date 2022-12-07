8b07d0839a294d5087505d3d8ce4c70751127de3
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-07 10:48:14 +0100

    Clean up

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,25 +1,3 @@
-def consume(itr):
-    try:
-        command = next(itr).strip()
-    except StopIteration:
-        return
-    if not command.startswith("$"):
-        return
-    output = []
-    while True:
-        try:
-            line = next(itr).strip()
-        except StopIteration:
-            yield command, output
-            return
-        if line.startswith("$"):
-            yield command, output
-            command = line
-            output = []
-        else:
-            output.append(line)
-
-
 def disk_usage(filesystem, res=None):
     res = [] if res is None else res
     size = 0
@@ -37,20 +15,19 @@ root = {}
 cwd = [root]
 
 with open("input07.txt", "r") as fobj:
-    for command, output in consume(fobj):
-        if command == "$ cd ..":
+    for line in fobj:
+        line = line.strip()
+        if line == "$ cd ..":
             cwd.pop()
-        elif command == "$ cd /":
+        elif line == "$ cd /":
             cwd = [root]
-        elif command.startswith("$ cd "):
-            cwd.append(cwd[-1].setdefault(command[5:], {}))
-        else:
-            for line in output:
-                if line.startswith("dir "):
-                    cwd[-1].setdefault(line[4:], {})
-                else:
-                    size, name = line.split()
-                    cwd[-1][name] = int(size)
+        elif line.startswith("$ cd "):
+            cwd.append(cwd[-1].setdefault(line[5:], {}))
+        elif line.startswith("dir "):
+            cwd[-1].setdefault(line[4:], {})
+        elif line != "$ ls":
+            size, name = line.split()
+            cwd[-1][name] = int(size)
 
 du = disk_usage(root)
 print(sum(i for i in du if i < 100_000))
