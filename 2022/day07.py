f611ca8dc295049e0625f693e0ae93ac18044553
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-07 15:58:18 +0100

    Either worse or better

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,18 +1,9 @@
-def disk_usage(filesystem, res=None):
-    res = [] if res is None else res
-    size = 0
-    for val in filesystem.values():
-        if isinstance(val, int):
-            size += val
-        else:
-            disk_usage(val, res=res)
-            size += res[-1]
-    res.append(size)
+def du(dirs, res):
+    res.append(sum(v if isinstance(v, int) else du(v, res)[-1] for v in dirs.values()))
     return res
 
 
-root = {}
-cwd = [root]
+cwd = [root := {}]
 
 with open("input07.txt", "r") as fobj:
     for line in fobj:
@@ -23,13 +14,10 @@ with open("input07.txt", "r") as fobj:
             cwd = [root]
         elif line.startswith("$ cd "):
             cwd.append(cwd[-1].setdefault(line[5:], {}))
-        elif line.startswith("dir "):
-            cwd[-1].setdefault(line[4:], {})
-        elif line != "$ ls":
+        elif not line.startswith("dir ") and line != "$ ls":
             size, name = line.split()
             cwd[-1][name] = int(size)
 
-du = disk_usage(root)
-print(sum(i for i in du if i < 100_000))
-threshold = du[-1] - 40_000_000
-print(min(i for i in du if i > threshold))
+res = du(root, [])
+print(sum(i for i in res if i < 100_000))
+print(min(i for i in res if i > res[-1] - 40_000_000))
