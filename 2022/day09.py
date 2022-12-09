6d4d777ebe1a20e49322919427c6b9102f79c889
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-09 11:18:48 +0100

    Shorter == better ?

--- 2022/day09.py
+++ 2022/day09.py
@@ -1,35 +1,16 @@
-directions = {"U": (0, 1), "R": (1, 0), "D": (0, -1), "L": (-1, 0)}
+def step(d):
+    return (max(min(d.real, 1), -1) + 1j * max(min(d.imag, 1), -1)) * int(abs(d) >= 2)
 
 
-def sign(x):
-    if x > 0:
-        return 1
-    if x < 0:
-        return -1
-    return 0
+def update(s):
+    rope[0] += s
+    rope[1:] = map(lambda x: x[1] + step(x[0] - x[1]), zip(rope, rope[1:]))
+    return rope[-1]
 
 
-def step(head, tail):
-    x = head[0] - tail[0]
-    y = head[1] - tail[1]
-    if x in (-1, 0, 1) and y in (-1, 0, 1):
-        return 0, 0
-    return sign(x), sign(y)
-
-
-def simulation(n, fobj):
-    rope = [(0, 0) for _ in range(n)]
-    visited = {rope[-1]}
-    for line in fobj:
-        key, steps = line.split()
-        for _ in range(int(steps)):
-            rope[0] = tuple(map(sum, zip(rope[0], directions[key])))
-            for i in range(1, len(rope)):
-                rope[i] = tuple(map(sum, zip(rope[i], step(rope[i - 1], rope[i]))))
-            visited.add(rope[-1])
-    print(len(visited))
-
+with open("input09.txt", "r") as fobj:
+    steps = [i for line in fobj for i in [1j ** "RULD".index(line[0])] * int(line[2:])]
 
 for n in (2, 10):
-    with open("input09.txt", "r") as fobj:
-        simulation(n, fobj)
+    rope = [0] * n
+    print(len(set(map(update, steps))))
