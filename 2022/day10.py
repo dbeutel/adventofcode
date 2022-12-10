a7ef4e4b9b18984bc466ef299c2b3e98e372e617
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-10 15:57:53 +0100

    'God lines' not 'Good lines'

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,11 +1,12 @@
-with open("input10.txt", "r") as fobj:
-    reg = [1]
-    for line in fobj:
-        reg.append(reg[-1])
-        if line != "noop\n":
-            reg.append(reg[-1] + int(line.split()[1]))
-print(sum((c * 40 + 20) * r for c, r in enumerate(reg[19::40])))
+from itertools import accumulate
+from textwrap import fill
 
-screen = "".join("#" if r - 1 <= c % 40 <= r + 1 else " " for c, r in enumerate(reg))
-for i in range(6):
-    print(screen[i * 40 : (i + 1) * 40])
+with open("input10.txt") as fobj:
+    reg = list(
+        accumulate(
+            map(int, fobj.read().replace("noop", "0").replace("addx", "0").split()),
+            initial=1,
+        )
+    )
+print(sum((c * 40 + 20) * r for c, r in enumerate(reg[19::40])))
+print(fill("".join("#" if abs(c % 40 - r) < 2 else " " for c, r in enumerate(reg)), 40))
