748a394dc84a77c691780cf0bdce6a0e3dd31cfb
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-13 13:52:32 +0100

    Even shorter now

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,21 +1,10 @@
-from ast import literal_eval
-from functools import cmp_to_key
-from itertools import zip_longest
-
-
 def cmp(a, b):
-    for x, y in zip_longest(a, b):
-        if None in (x, y):
-            return 1 - 2 * int(x is None)
-        if isinstance(x, int) and isinstance(y, int):
-            if x != y:
-                return 1 if x > y else -1
-        elif (val := cmp(*([i] if isinstance(i, int) else i for i in (x, y)))) != 0:
-            return val
-    return 0
+    a, b = ab = [[i] if isinstance(i, int) else i for i in (a, b)]
+    if all(isinstance(i, int) for i in a + b):
+        return (a > b) - (a < b)
+    return x if (x := cmp(*(i[0] if len(i) else i for i in ab))) else cmp(a[1:], b[1:])
 
 
-sig = list(map(literal_eval, open("input13.txt").read().split()))
-print(sum(i + 1 for i, (a, b) in enumerate(zip(sig[::2], sig[1::2])) if cmp(a, b) < 0))
-sig = sorted(sig + [[[2]], [[6]]], key=cmp_to_key(cmp))
-print((sig.index([[2]]) + 1) * (sig.index([[6]]) + 1))
+s = list(map(__import__("ast").literal_eval, open("input13.txt").read().split()))
+print(sum(i + 1 for i, (a, b) in enumerate(zip(s[::2], s[1::2])) if cmp(a, b) < 0))
+print((sum(cmp(i, [[2]]) < 0 for i in s) + 1) * (sum(cmp(i, [[6]]) < 0 for i in s) + 2))
