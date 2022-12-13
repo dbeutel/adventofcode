1fe3fc6d23bdcd67408bd4b218ea05e453947ffd
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-13 09:44:38 +0100

    Found out about cmp_to_key

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,39 +1,20 @@
+from functools import cmp_to_key
 from itertools import zip_longest
 
 
-def compare(a, b):
+def cmp(a, b):
     for x, y in zip_longest(a, b):
         if None in (x, y):
-            return x is None
+            return 1 - 2 * int(x is None)
         if isinstance(x, int) and isinstance(y, int):
-            if x > y:
-                return False
-            if x < y:
-                return True
-        else:
-            x = [x] if isinstance(x, int) else x
-            y = [y] if isinstance(y, int) else y
-            if (comp := compare(x, y)) is not None:
-                return comp
-    return None
+            if x != y:
+                return 1 if x > y else -1
+        elif (val := cmp(*([i] if isinstance(i, int) else i for i in (x, y)))) != 0:
+            return val
+    return 0
 
 
-def quicksort(x):
-    if len(x) <= 1:
-        return x
-    pivot = x[0]
-    before = []
-    after = []
-    for i in x[1:]:
-        if compare(i, pivot):
-            before.append(i)
-        else:
-            after.append(i)
-    return quicksort(before) + [pivot] + quicksort(after)
-
-
-sig = [eval(i) for i in open("input13.txt").read().split()]  # too tempting to use eval
-print(sum(i + 1 for i, (a, b) in enumerate(zip(sig[::2], sig[1::2])) if compare(a, b)))
-sig.extend([[[2]], [[6]]])
-sig = quicksort(sig)
+sig = list(map(eval, open("input13.txt").read().split()))  # too tempting to use eval
+print(sum(i + 1 for i, (a, b) in enumerate(zip(sig[::2], sig[1::2])) if cmp(a, b) < 0))
+sig = sorted(sig + [[[2]], [[6]]], key=cmp_to_key(cmp))
 print((sig.index([[2]]) + 1) * (sig.index([[6]]) + 1))
