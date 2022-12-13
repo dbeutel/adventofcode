9d78c83dbfa72764e493a63960fc88dca8065e2a
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-13 09:42:45 +0100

    A* algorithm

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,34 +1,51 @@
+import bisect
+
+# Commented lines are for the backwards search, which visits fewer nodes for the typical
+# input from Advent of Code, but the heuristics makes it quite slow in part 2.
+
+
+def heuristics(z, goal):
+    return min(abs((z - g).real) + abs((z - g).imag) for g in goal)
+
+
+def pathlength(grid, check, goal):
+    dists = {pos: 0 for pos in check}
+    visited = set()
+    while check:
+        pos = check.pop(0)
+        dist = dists.pop(pos)
+        if pos in goal:
+            return dist
+        visited.add(pos)
+        for i in range(4):
+            npos = pos + 1j**i
+            if (
+                npos not in visited
+                and grid.get(npos, grid[pos] + 2) <= grid[pos] + 1
+                # and grid.get(npos, grid[pos] - 2) >= grid[pos] - 1
+                and dists.get(npos, dist + 2) > dist + 1
+            ):
+                if npos in check:
+                    check.remove(npos)
+                dists[npos] = dist + 1
+                bisect.insort(check, npos, key=lambda x: dists[x] + heuristics(x, goal))
+
+
 grid = {}
 with open("input12.txt") as fobj:
-    for i, line in enumerate(fobj):
-        for j, c in enumerate(line.strip()):
-            if c == "S":
-                check = {j - i * 1j}
-                grid[j - i * 1j] = 1
-            elif c == "E":
-                goal = j - i * 1j
-                grid[j - i * 1j] = 26
+    for row, line in enumerate(fobj):
+        for col, char in enumerate(line.strip()):
+            if char == "S":
+                start = col - row * 1j
+                grid[col - row * 1j] = 1
+            elif char == "E":
+                end = col - row * 1j
+                grid[col - row * 1j] = 26
             else:
-                grid[j - i * 1j] = ord(c) - 96
+                grid[col - row * 1j] = ord(char) - 96
 
 
-def pathlengths(grid, check):
-    dists = {pos: 0 for pos in check}
-    while check:
-        pos = check.pop()
-        dist = dists[pos]
-        for i in range(4):
-            nextpos = pos + 1j**i
-            try:
-                nextval = grid[nextpos]
-            except KeyError:
-                continue
-            if nextval <= grid[pos] + 1 and dists.get(nextpos, dist + 2) > dist + 1:
-                dists[nextpos] = dist + 1
-                check.add(nextpos)
-    return dists
-
-
-print(pathlengths(grid, check)[goal])
-check = {pos for pos, val in grid.items() if val == 1}
-print(pathlengths(grid, check)[goal])
+print(pathlength(grid, [start], (end,)))
+# print(pathlength(grid, [end], (start,)))
+print(pathlength(grid, [pos for pos, val in grid.items() if val == 1], (end,)))
+# print(pathlength(grid, [end], tuple(pos for pos, val in grid.items() if val == 1)))
