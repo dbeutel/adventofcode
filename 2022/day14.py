d822a425219610b59e8e7ea3663e3250491bf17a
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-14 09:59:54 +0100

    Reuse result from part 1

--- 2022/day14.py
+++ 2022/day14.py
@@ -10,21 +10,19 @@ def run(rocks, until, threshold):
                 x, y = x + dx, y + 1
                 break
         else:
-            rocks = rocks | {(x, y)}
+            rocks.add((x, y))
             x, y = 500, 0
     return rocks
 
 
 rocks = set()
-maxy = -1
 with open("input14.txt") as fobj:
     for line in fobj:
         corners = [*map(ast.literal_eval, line.split(" -> "))]
         for c in zip(corners, corners[1:]):
-            maxy = max(maxy, c[0][1], c[1][1])
             rocks |= {*product(*map(lambda p: range(min(p), max(p) + 1), zip(*c)))}
 
 nrocks = len(rocks)
 maxy = max(map(lambda i: i[1], rocks))
 print(len(run(rocks, lambda y, _: y <= maxy, lambda y: True)) - nrocks)
-print(len(run(rocks, lambda _, r: (500, 0) not in r, lambda y: y < maxy + 1)) - nrocks)
+print(len(run(rocks, lambda _, r: (500, 0) not in r, lambda y: y <= maxy)) - nrocks)
