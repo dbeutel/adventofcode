94ef6f8915d78e0cb4381ead750b2cd23b0b1e85
Dominik Beutel <dominik.beutel@kit.edu>
2022-12-20 09:03:00 +0100

    Remove global variable

--- 2022/day16.py
+++ 2022/day16.py
@@ -18,7 +18,7 @@ def dijkstra(nodes, edges, current):
     return dists
 
 
-def run(dists, nodes, status=0, current="AA", time=26, flow=0, res=None):
+def run(dists, nodes, indices, status=0, current="AA", time=26, flow=0, res=None):
     res = {} if res is None else res
     res[status] = max(res.get(status, 0), flow)
     for node, rate in nodes.items():
@@ -26,7 +26,7 @@ def run(dists, nodes, status=0, current="AA", time=26, flow=0, res=None):
         if index & status or (ntime := time - 1 - dists[current][node]) <= 0:
             continue
         fom = ntime * rate + flow
-        run(dists, nodes, index | status, node, ntime, fom, res)
+        run(dists, nodes, indices, index | status, node, ntime, fom, res)
     return res
 
 
@@ -34,22 +34,22 @@ regex = re.compile(
     r"^Valve ([A-Z]{2}) has flow rate=([0-9]+); "
     r"(?:tunnel leads|tunnels lead) to valve(?:s?) ([A-Z]{2}(?:, [A-Z]{2})*)\n$"
 )
-nodes = {}
+nds = {}
 edges = {}
 with open("input16.txt") as fobj:
     for line in fobj:
         name, rate, tunnels = regex.match(line).groups()
         rate = int(rate)
-        nodes[name] = int(rate)
+        nds[name] = int(rate)
         for tunnel in tunnels.split(", "):
             edges.setdefault(name, {})[tunnel] = 1
-dists = {
-    node: dijkstra(nodes, edges, node)
-    for node, rate in nodes.items()
+dsts = {
+    node: dijkstra(nds, edges, node)
+    for node, rate in nds.items()
     if node == "AA" or rate > 0
 }
-nodes = dict(filter(lambda i: i[1] != 0 or i[0] == "AA", nodes.items()))
-indices = {a: 1 << i for i, a in enumerate(dists)}
+nds = dict(filter(lambda i: i[1] != 0 or i[0] == "AA", nds.items()))
+ixs = {a: 1 << i for i, a in enumerate(dsts)}
 
-print(max(run(dists, nodes, time=30).values()))
-print(max(i + j for (a, i), (b, j) in com(run(dists, nodes).items(), 2) if not a & b))
+print(max(run(dsts, nds, ixs, time=30).values()))
+print(max(i + j for (a, i), (b, j) in com(run(dsts, nds, ixs).items(), 2) if not a & b))
